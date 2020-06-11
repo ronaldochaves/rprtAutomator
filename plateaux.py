2720973b74de65e496a87f32b322c9b33f17c37c
ronaldochaves <ronald.chaves15@gmail.com>
2020-06-11 00:44:17 -0300

    The implementation for the centralized method is over. However, the code can be cleaner. The subindexing of np.array or list does not account the last element, making dynamic subindexing kinda tricky. Anyways, I will start to implement the distributed method.

--- plateaux.py
+++ plateaux.py
@@ -40,7 +40,10 @@ def find_left_plateau(A, L, m, tau):
 		min_r = min_right(A, l, m)
 		max_l = max_left(A, L, l, m)
 		if min_r >= max_l and min_r >= tau:
-			tau_l = max(tau, max(A[L:l + 1]))
+			if l == L:
+				tau_l = max(tau, A[l])
+			else:
+				tau_l = max(tau, max(A[L:l]))
 			print ('l = {}, m = {}, tau_l = {}'.format(l, m, tau_l))
 			return l, m, tau_l
 	return print('Error: left plateau not found!')
@@ -53,7 +56,10 @@ def find_right_plateau(A, m, R, tau):
 		max_r = max_right(A, m, r, R)
 		print(max_r)
 		if min_l >= max_r and min_l >= tau:
-			tau_r = max(tau, max(A[r + 1:R + 1]))
+			if r == R:
+				tau_r = max(tau, A[r])
+			else:
+				tau_r = max(tau, max(A[r + 1:R + 1]))
 			print ('m = {}, r = {}, tau_r = {}'.format(m, r, tau_r))
 			return m, r, tau_r
 	return print('Error: left plateau not found!')
@@ -62,41 +68,44 @@ def min_right(A, i, m):
 	if i == m:
 		return A[m]
 	else:
-		return min(A[i:m])
+		return min(A[i:m + 1])
 
 def max_left(A, L, i, m):
 	if i == L:
 		return -np.inf
 	else:
-		return max(A[:i])
+		return max(A[L:i])
 
 def min_left(A, m, i):
 	if i == m:
 		return A[m]
 	else:
-		print(A[m:i+1])
-		return min(A[m:i+1])
+		# print(A[m:i + 1])
+		return min(A[m:i + 1])
 
 def max_right(A, m, i, R):
 	if i == R:
 		return -np.inf
 	else:
-		print(A[i:])
-		return max(A[i:])
+		# print(A[i + 1:R + 1])
+		return max(A[i + 1:R + 1])
 
 # Distributed Algorithm ro find plateau (Threshold Algorithm by Fagin et al.)# Test case
 
 
 # Test case
 if __name__ == '__main__':
-	#A = np.array([0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 3, 2, 1, 0])
-	#A = np.array([0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 3, 2, 1, 0, 5, 5, 5, 5, 5 ,5, 5])
-	#A = np.array([3, 3, 5, 3, 3, 0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 3, 2, 1, 0, 5, 5, 5, 5, 5 ,5, 5])
+	A = np.array([1, 5, 0, 4, 7, 3, 6, 8, 10, 12, 9, 2])
+	# A = np.array([0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 3, 2, 1, 0])
+	# A = np.array([0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 3, 2, 1, 0, 5, 5, 5, 5, 5 ,5, 5])
+	# A = np.array([3, 3, 5, 3, 3, 0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 3, 2, 1, 0, 5, 5, 5, 5, 5 ,5, 5])
 	# A = np.array([3, 3, 5, 3, 3, 0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 3, 2, 1, 0, 5, 5, 5, 5, 5 ,5, 5, 6])
-	A = np.array([6, 3, 3, 5, 3, 3, 0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 3, 2, 1, 0, 5, 5, 5, 5, 5 ,5, 5, 6])
+	# A = np.array([6, 3, 3, 5, 3, 3, 0, 1, 2, 3, 4, 5, 5, 5, 5, 5, 5, 5, 5, 4, 3, 2, 3, 2, 1, 0, 5, 5, 5, 5, 5 ,5, 5, 6])
 	
 	epson = 0.5
 	plateau_l, plateau_r, tau = find_plateau(A, epson)
+
+	print('Plateau: ({}, {}) - tau: {}'.format(plateau_l, plateau_r, tau))
 
 	fig = plt.figure('Debug-Plateau', figsize = (10, 6), dpi = 80)
 	plt.plot(A, color = 'red', linewidth = 2, marker = 's', label = 'A')
@@ -108,5 +117,3 @@ if __name__ == '__main__':
 	plt.xlabel("Index")
 	plt.ylabel("Aggregated Time Series")
 	plt.show()
-
-	print('Plateau: ({}, {}) - tau: {}'.format(plateau_l, plateau_r, tau))
