56073bde40e1a8bf833ff13e417a902621930f34
Ronaldo Chaves <ronald.chaves15@gmail.com>
2020-07-14 19:59:31 -0300

    Small fix forgoten from last sprint

--- tests/test_all.py
+++ tests/test_all.py
@@ -4,7 +4,7 @@ import os
 dir_test = os.path.dirname(__file__)
 
 for f in sorted(os.listdir(dir_test)):
-    if f != os.path.basename(__file__):
+    if f != os.path.basename(__file__) and os.path.isfile(f):
         exec(open(os.path.join(dir_test, f)).read())
 
 print('Finished', __file__)
