361ca846efa6892dc510a5ea5393b54b9309da3e
Ronaldo Chaves <ronald.chaves15@gmail.com>
2020-07-15 01:18:26 -0300

    Never give up

--- tests/test_all.py
+++ tests/test_all.py
@@ -7,8 +7,8 @@ print("Starting all tests with", os.path.realpath(__file__))
 dir_test = os.path.dirname(os.path.realpath(__file__))
 
 for f in sorted(os.listdir(dir_test)):
-    print(f)
     if f != os.path.basename(os.path.realpath(__file__)) and os.path.isfile(f) and not f.startswith('.'):
+        print('Running:', f)
         exec(open(f).read())
 
 print('Finished', os.path.realpath(__file__))
