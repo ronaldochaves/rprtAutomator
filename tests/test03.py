611fb1877b403d23c87d4112679951e11848019e
Ronaldo Chaves <ronald.chaves15@gmail.com>
2020-07-15 01:27:36 -0300

    Never give up again

--- tests/test03.py
+++ tests/test03.py
@@ -4,7 +4,7 @@ import os
 # Local imports
 from tools import createdocument
 
-testDoc = createdocument.ReportDocument()
+testDoc = createdocument.ReportDocument(file_name_prefix='test03_report_v')
 testDoc.finish()
 
 print('Finished', os.path.realpath(__file__))
--- tests/test_all.py
+++ tests/test_all.py
@@ -7,6 +7,10 @@ print("Starting all tests with", os.path.realpath(__file__))
 dir_test = os.path.dirname(os.path.realpath(__file__))
 
 for f in sorted(os.listdir(dir_test)):
+    print('Conditions for file:', f)
+    print('f != os.path.basename(os.path.realpath(__file__))', f != os.path.basename(os.path.realpath(__file__)))
+    print('os.path.isfile(f)', os.path.isfile(f))
+    print("not f.startswith('.')", not f.startswith('.'))
     if f != os.path.basename(os.path.realpath(__file__)) and os.path.isfile(f) and not f.startswith('.'):
         print('Running:', f)
         exec(open(f).read())
