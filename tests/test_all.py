47d9d7bd08a2389131552766fef6337c5ed03eba
Ronaldo Chaves <ronald.chaves15@gmail.com>
2020-07-15 01:34:04 -0300

    @RianKoja is sleepy

--- tests/test_all.py
+++ tests/test_all.py
@@ -9,9 +9,9 @@ dir_test = os.path.dirname(os.path.realpath(__file__))
 for f in sorted(os.listdir(dir_test)):
     print('Conditions for file:', f)
     print('f != os.path.basename(os.path.realpath(__file__))', f != os.path.basename(os.path.realpath(__file__)))
-    print('os.path.isfile(f)', os.path.isfile(f))
+    print('os.path.isfile(os.path.join(dir_test, f))', os.path.isfile(os.path.join(dir_test, f)))
     print("not f.startswith('.')", not f.startswith('.'))
-    if f != os.path.basename(os.path.realpath(__file__)) and os.path.isfile(f) and not f.startswith('.'):
+    if f != os.path.basename(os.path.realpath(__file__)) and os.path.isfile(os.path.join(dir_test, f)) and not f.startswith('.'):
         print('Running:', f)
         exec(open(f).read())
 
