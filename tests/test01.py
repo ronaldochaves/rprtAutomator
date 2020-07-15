039993daa37b750e79ea9b70432745a8ba72f6f9
Ronaldo Chaves <ronald.chaves15@gmail.com>
2020-07-15 05:08:02 -0300

    Removed __init__ and moved append to test_all.py

--- tests/test01.py
+++ tests/test01.py
@@ -1,8 +1,8 @@
 # Standard imports
 import hashlib
 import os
-import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+# import sys
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 # Local imports
 import genTestFile
--- tests/test_all.py
+++ tests/test_all.py
@@ -1,5 +1,7 @@
 # Standard imports
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 print("Starting the test suite", os.path.realpath(__file__))
 
