b9f524069d3bfaae50ced6725eeffece8a6ef56e
RianKoja <riankoja@gmail.com>
2020-07-15 00:12:28 -0300

    modified handling of test calls in test_all.py

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,5 +1,6 @@
 # Standard imports
 import os
+import subprocess
 
 dir_test = os.path.dirname(os.path.realpath(__file__))
 print('dir_test:', dir_test)
@@ -8,6 +9,15 @@ print('os.listdir(dir_test)', os.listdir(dir_test))  # Debugging on GitHub
 for f in sorted(os.listdir(dir_test)):
     print(f)
     if f != os.path.basename(os.path.realpath(__file__)) and os.path.isfile(f) and not f.startswith('.'):
-        exec(open(os.path.join(dir_test, f)).read())
+        ret = os.system("python " + os.path.join(dir_test, f))
+        print(ret)
+        proc = subprocess.Popen(
+            ["python", f], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc.wait()
+        (stdout, stderr) = proc.communicate()
+
+        if proc.returncode != 0:
+            print(stderr)
+            raise Exception('ExecutionError', f)
 
 print('Finished', os.path.realpath(__file__))
