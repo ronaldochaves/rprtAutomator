812ee2b5fb8b91430523f5b8ece162f0d2a4bea4
Ronaldo Chaves <ronald.chaves15@gmail.com>
2020-07-14 22:19:15 -0300

    FIxed routine to run all test files

--- tests/test01.py
+++ tests/test01.py
@@ -13,7 +13,7 @@ def check_hash(file, std_hash):
     print(file_hash)
 
     # Check integrity
-    # assert file_hash == std_hash
+    assert file_hash == std_hash
 
 
 # Specify input and output folders
@@ -21,16 +21,19 @@ data_dir_input = os.path.join(os.path.dirname(__file__), 'test_inputs')
 data_dir_output = os.path.join(os.path.dirname(__file__), 'test_outputs')
 
 # Check input
-file2_name = 'rack01_time_stamp.tdms'
-file2 = os.path.join(data_dir_input, file2_name)
-std_hash_input = '9264aa3ff8084135fc38d7c6ad861ddf1e06b948'
-check_hash(file2, std_hash_input)
+file_names = ['example_matfile.mat', 'example_01.tdms', 'example_02.tdms', 'example_03.tdms']
+files = [os.path.join(data_dir_input, f) for f in file_names]
+
+std_hash_inputs = ['8c7dc7c2085da896e5ca1a9297c23a25525d3e4d', 'f7533d65629cb58c22897cb8b4490e7ed98c560d',
+                   '2430171163f7ee53224b8bb427599c2cdac542df', '20867a62b44205c89d0af010af869216c6771956']
+for f, hash_input in zip(files, std_hash_inputs):
+    check_hash(f, hash_input)
 
 # Execute function under test
-genTestFile.main(data_dir_input, data_dir_output, file2)
+genTestFile.main(data_dir_output, files)
 
 # Check output
 file_name_output = 'DSapp_Test.csv'
 file_output = os.path.join(data_dir_output, file_name_output)
-std_hash_output = '9e9259ea2fa145adf07fad61c3b12df63f2a3b5a'
+std_hash_output = 'a155e0495bf457abe4c585df23b66a1a3e6069ba'
 check_hash(file_output, std_hash_output)
--- tests/test_all.py
+++ tests/test_all.py
@@ -4,7 +4,8 @@ import os
 dir_test = os.path.dirname(__file__)
 
 for f in sorted(os.listdir(dir_test)):
-    if f != os.path.basename(__file__) and os.path.isfile(f):
+    print(f)
+    if f != os.path.basename(__file__) and os.path.isfile(f) and not f.startswith('.'):
         exec(open(os.path.join(dir_test, f)).read())
 
 print('Finished', __file__)
