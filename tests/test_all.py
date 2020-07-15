25a44f2dc57e7ddac54363b9ce35d8e992a9b1e4
RianKoja <riankoja@gmail.com>
2020-07-15 01:08:43 -0300

    debug attempt, using exec instead of subprocess

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,19 +1,14 @@
 # Standard imports
 import os
-import sys
-import subprocess
+import importlib
+
+print("Starting all tests with", os.path.realpath(__file__))
 
 dir_test = os.path.dirname(os.path.realpath(__file__))
 
 for f in sorted(os.listdir(dir_test)):
     print(f)
     if f != os.path.basename(os.path.realpath(__file__)) and os.path.isfile(f) and not f.startswith('.'):
-        proc = subprocess.Popen([sys.executable, f], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        proc.wait()
-        (stdout, stderr) = proc.communicate()
-        print(stdout.decode('utf-8'))
-        if proc.returncode != 0:
-            print(stderr.decode('utf-8'))
-            raise Exception('ExecutionError', f)
+        exec(open(f).read())
 
 print('Finished', os.path.realpath(__file__))
