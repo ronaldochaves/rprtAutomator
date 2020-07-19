21cb6b2a7402293b3cfc1002cef4219d00d704fb
ronaldochaves <ronald.chaves15@gmail.com>
2020-07-19 07:30:51 -0300

    Added explanation of check_hash and verbose feature

--- tools/check_hash.py
+++ tools/check_hash.py
@@ -2,6 +2,10 @@
 import hashlib
 
 def check_hash(file, hash_list, verbose=False):
+    """
+    Check hash of a file and compare with hashes from a list (POSIX and Windows). Verbose enables printing hash during
+    tests.
+    """
     fd = open(file, 'rb')  # Read in binary mode to avoid importing npTDMS library
     sha1 = hashlib.sha1(fd.read())
     file_hash = sha1.hexdigest()
